61ac2d4a1ca63ca4486fa53521b2bb597a3c5cac
ajarrott <mrajarrott@gmail.com>
2015-02-08 01:51:58 -0800

    newstuf

--- FunctionCalls/FunctionCalls/Program.cs
+++ FunctionCalls/FunctionCalls/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace FunctionCalls
 {
@@ -10,10 +11,31 @@ namespace FunctionCalls
     {
         static void Main(string[] args)
         {
+            List<string> l = new List<string>();
+            List<string> toPrint;// = new List<string>();
+
+            l.Add("asdfasdfasdf");
+            l.Add("tester");
+            l.Add("C:\testagainwith\a\folder");
+
             parser p = new parser();
             functions f = new functions(p.get_user_def);
+            SearchStrings s = new SearchStrings(l);
 
             f.ReturnAndRunFunc("lol");
+
+            StringBuilder str = new StringBuilder();
+            char c = '\0';
+
+            while ( c != '\n' )
+            {
+                str.Append(Console.Read());
+                toPrint = s.DisplayStringsFromInput(str.ToString());
+                foreach(string asdf in toPrint)
+                {
+                    Console.WriteLine(asdf);
+                }
+            }
             Console.ReadLine();
         }
     }
---
+++ FunctionCalls/FunctionCalls/SearchStrings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace FunctionCalls
+{
+    class SearchStrings
+    {
+        List<string> listToSearch;
+        Regex r;
+        
+        public SearchStrings(List<string> passedStrings)
+        {
+            listToSearch = passedStrings;
+        }
+
+        public List<string> DisplayStringsFromInput(string input)
+        {
+            List<string> listToReturn = new List<string>();
+            string regexString = "*" + input + "*";
+            r = new Regex(regexString);
+
+            foreach( string s in listToSearch )
+            {
+                if (r.IsMatch(s))
+                {
+                    listToReturn.Add(s);
+                }
+            }
+
+
+            return listToReturn;
+        }
+    }
+}
